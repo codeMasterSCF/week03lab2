6ca363426b41b0cafcd4d55f93582c57e74b47cf
463849 <463849@ELF6IT215756.ACDM.DS.SAIT.CA>
2017-09-27 14:48:14 -0600

    Finished Logic Finished logic for Servlet

--- src/java/calculations/CalculatorServlet.java
+++ src/java/calculations/CalculatorServlet.java
@@ -1,12 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package calculations;
 
 import java.io.IOException;
-import java.io.PrintWriter;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -22,12 +17,6 @@ public class CalculatorServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
         String firstStr = request.getParameter("first");
@@ -60,7 +49,17 @@ public class CalculatorServlet extends HttpServlet {
 
                 calcPerformed = true;
             }
+            request.setAttribute("result", result);
+            getServletContext().getRequestDispatcher("/WEB-INF/calculator.jsp").forward(request, response);
         }
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        
+        
 
     }
 
--- web/WEB-INF/calculator.jsp
+++ web/WEB-INF/calculator.jsp
@@ -5,21 +5,19 @@
 --%>
 
 <%@page contentType="text/html" pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <c:import url="/WEB-INF/header.html" />
 
         <h1>Simple JSP Calculator</h1>
         
-        <form action="index.jsp" method="GET">
+        <form action="CalculatorServlet" method="GET">
             First: <input type="text" name="first"><br/>
             Second: <input type="text" name="second"><br/>
             <input type="submit" name="operation" value="+"> <input type="submit" name="operation" value="-"> 
             <input type="submit" name="operation" value="*"> <input type="submit" name="operation" value="%">
         </form>
         <br/>
-        <div>
-            <p>result: ${calcPerformed}</p>
-        </div>
-        
+        <div>Result: ${result}</div>
         
 <c:import url="/WEB-INF/footer.html" />
  
